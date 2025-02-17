9dbb1f7d48feec7ba44346ee83dd6a36669aec6a
NITHARD Damien <damien.nithard@assurance-maladie.fr>
2025-02-17 15:38:46 +0100

    Rename to InitializerConfig and delete unused properties

--- src/main/java/fr/cnam/ddst/config/InitializerConfig.java
+++ src/main/java/fr/cnam/ddst/config/InitializerConfig.java
@@ -5,7 +5,6 @@ import lombok.Setter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 
-import java.util.List;
 import java.util.Map;
 
 /**
@@ -22,22 +21,12 @@ import java.util.Map;
 @Getter
 @Configuration
 @ConfigurationProperties(prefix = "initializer")
-public class InitializerProperties {
+public class InitializerConfig {
 
     /**
      * Configuration spécifique à l'instanciateur TONIC.
      */
     private Map<String, Object> tonic;
-
-    /**
-     * Configuration spécifique à l'instanciateur HUMAN.
-     */
-    private Map<String, Object> human;
-
-    /**
-     * Mapping des fonctionnalités disponibles pour chaque type d'instanciateur.
-     */
-    private Map<String, List<String>> features;
 
     /**
      * Récupère l'URL de l'instanciateur TONIC.
@@ -48,22 +37,5 @@ public class InitializerProperties {
         return (String) tonic.get("url");
     }
 
-    /**
-     * Récupère l'URL de l'instanciateur HUMAN.
-     *
-     * @return L'URL de l'instanciateur HUMAN
-     */
-    public String getHumanUrl() {
-        return (String) human.get("url");
-    }
 
-    /**
-     * Récupère la liste des fonctionnalités disponibles pour un type d'instanciateur donné.
-     *
-     * @param type Le type d'instanciateur (TONIC, HUMAN)
-     * @return La liste des fonctionnalités disponibles pour ce type
-     */
-    public List<String> getFeaturesForType(String type) {
-        return features.get(type.toLowerCase());
-    }
 }
